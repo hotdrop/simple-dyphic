cafdb3fadd2ddf3c20205b3063f1fdda639e879b
kenji <hotdrop.tkenji@gmail.com>
2025-02-01 12:14:53 +0900

    hiveのマイグレーションコードはもう不要なので削除

--- lib/repository/local/record_dao.dart
+++ lib/repository/local/record_dao.dart
@@ -1,10 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:isar/isar.dart';
-import 'package:simple_dyphic/common/app_logger.dart';
 import 'package:simple_dyphic/model/record.dart';
 import 'package:simple_dyphic/repository/local/entities/record_entitiy_isar.dart';
-import 'package:simple_dyphic/repository/local/entities/record_entity.dart';
 import 'package:simple_dyphic/repository/local/local_data_source.dart';
 
 final recordDaoProvider = Provider((ref) => _RecordDao(ref));
@@ -44,78 +41,6 @@ class _RecordDao {
       await isar.clear();
       await isar.recordEntitiyIsars.putAll(entities);
     });
-  }
-
-  // 以下、Hive用のメソッド。後ほど消す
-  Future<List<Record>> findAllForHive() async {
-    final box = await Hive.openBox<RecordEntity>(RecordEntity.boxName);
-    if (box.isEmpty) {
-      return [];
-    }
-
-    final entities = box.values;
-    AppLogger.d('ローカルからレコード一覧を取得しました。レコード数: ${entities.length}');
-
-    return entities
-        .map((e) => Record.create(
-              id: e.id,
-              breakfast: e.breakfast,
-              lunch: e.lunch,
-              dinner: e.dinner,
-              isToilet: e.isToilet,
-              condition: e.condition,
-              conditionMemo: e.conditionMemo,
-            ))
-        .toList();
-  }
-
-  Future<Record> findForHive(int key) async {
-    final box = await Hive.openBox<RecordEntity>(RecordEntity.boxName);
-    AppLogger.d('key=$key');
-    final e = box.get(key)!;
-
-    return Record.create(
-      id: e.id,
-      breakfast: e.breakfast,
-      lunch: e.lunch,
-      dinner: e.dinner,
-      isToilet: e.isToilet,
-      condition: e.condition,
-      conditionMemo: e.conditionMemo,
-    );
-  }
-
-  Future<void> saveForHive(Record record) async {
-    final entity = RecordEntity(
-      id: record.id,
-      breakfast: record.breakfast,
-      lunch: record.lunch,
-      dinner: record.dinner,
-      isToilet: record.isToilet,
-      condition: record.condition,
-      conditionMemo: record.conditionMemo,
-    );
-
-    final box = await Hive.openBox<RecordEntity>(RecordEntity.boxName);
-    await box.put(entity.id, entity);
-  }
-
-  Future<void> saveAllForHive(List<Record> records) async {
-    final entities = records
-        .map((r) => RecordEntity(
-              id: r.id,
-              breakfast: r.breakfast,
-              lunch: r.lunch,
-              dinner: r.dinner,
-              isToilet: r.isToilet,
-              condition: r.condition,
-              conditionMemo: r.conditionMemo,
-            ))
-        .toList();
-
-    final box = await Hive.openBox<RecordEntity>(RecordEntity.boxName);
-    await box.clear();
-    await box.addAll(entities);
   }
 
   Record _entityToModel(RecordEntitiyIsar entity) {
--- test/repository/test_local_data_source.dart
+++ test/repository/test_local_data_source.dart
@@ -1,17 +1,9 @@
 import 'dart:io';
 
-import 'package:hive_flutter/hive_flutter.dart';
-import 'package:simple_dyphic/repository/local/entities/record_entity.dart';
 import 'package:simple_dyphic/repository/local/local_data_source.dart';
 
 class TestLocalDataSource extends LocalDataSource {
   TestLocalDataSource(super.ref);
-
-  @override
-  Future<void> initHive() async {
-    Hive.init(Directory.current.path);
-    Hive.registerAdapter(RecordEntityAdapter());
-  }
 
   @override
   Future<String> getDirectoryPath() async {
