0d128cfce4d999df23de1ae7206b2b3b724dfe78
kenji <hotdrop.tkenji@gmail.com>
2025-02-02 10:49:02 +0900

    fix: 警告が出ていたcontextを無条件で非同期処理で使用していた箇所を修正

--- lib/ui/setting/widget/app_progress_dialog.dart
+++ lib/ui/setting/widget/app_progress_dialog.dart
@@ -4,22 +4,25 @@ import 'package:simple_dyphic/common/app_logger.dart';
 class AppProgressDialog<T> {
   const AppProgressDialog();
 
-  void show(
+  Future<void> show(
     BuildContext context, {
     required Future<T> Function() execute,
     required Function(T) onSuccess,
     required Function(Exception) onError,
-  }) {
+  }) async {
     _showProgressDialog(context);
     try {
-      execute().then((result) {
+      final result = await execute();
+      if (context.mounted) {
         Navigator.pop(context);
         onSuccess(result);
-      });
+      }
     } on Exception catch (e, s) {
       AppLogger.e('エラーが発生しました。', e, s);
-      Navigator.pop(context);
       onError(e);
+      if (context.mounted) {
+        Navigator.pop(context);
+      }
     }
   }
 
